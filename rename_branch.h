ac8a7bfdad3e82cc5e36e9ff13c83917d51b0f10
Telephone <telephone.tan@qq.com>
2020-10-15 00:53:50 +0800

    change rename_branch tip highlight color

--- rename_branch.h
+++ rename_branch.h
@@ -12,10 +12,10 @@
 int rename_branch(const args_table_type &args_table){
     std::cout << "rename_branch() called" << std::endl;
     arg args[] = {
-            {"-old-branch", "", "parameter required: " "\033[1;33m" "old-branch" "\033[0m" " " "the old branch to be renamed"},
-            {"-new-name", "", "parameter required: " "\033[1;33m" "new-name" "\033[0m" " " "the new branch name"},
-            {"-force", "", "parameter required: " "\033[1;33m" "force" "\033[0m" " "
-                                                                                 "Should it be forced to rename if a branch with the same name as the new name already exists?(" "\033[0;34m" "true/false" "\033[0m" ")"}
+            {"-old-branch", "", "parameter required: " "\033[0;33m" "old-branch" "\033[0m" " " "the old branch to be renamed"},
+            {"-new-name", "", "parameter required: " "\033[0;33m" "new-name" "\033[0m" " " "the new branch name"},
+            {"-force", "", "parameter required: " "\033[0;33m" "force" "\033[0m" " "
+                                                                                 "Should it be forced to rename if a branch with the same name as the new name already exists?(" "\033[1;34m" "true/false" "\033[0m" ")"}
     };
     for (auto &i : args_table){
         for (auto &j : args){
