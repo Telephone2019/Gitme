6a0aa1215756ff6e42750bc614f30ab51e7b1936
yjt <1624485653@qq.com>
2020-10-31 19:19:33 +0800

    remove some files

--- Completion/stash/pname.sh
+++
@@ -1,8 +0,0 @@
-gitme+([[:space:]])stash+([[:space:]])*(?)[^:] | gitme+([[:space:]])stash+([[:space:]]) )
-mapfile -t COMPREPLY < <(compgen -W "-help -show: -show:<stash@{$num}> -apply: -apply:<stash@{$num}> -pop: -pop:<stash@{$num}> drop: drop:<stash@{$num}> -save-with-message: -save-with-message:<commit-message> -list -list: -list:(true\\ |\\ false) -clear -clear: -clear:(true\\ |\\ false) -strict-mode -strict-mode: -strict-mode:(on\\ |\\ off)" -- "$cur");;
-
-
-
-
-
-
